21a321dfdd99284b9da24c915eef0135c263e217
Adrian <adrian.faciu@gmail.com>
2017-10-03 21:35:28 +0300

    Working on a more lib like structure

--- src/demo/app/app.module.ts
+++ src/demo/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxRetriableModule } from '../../retriable-src/retriable-src.module';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxRetriableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
