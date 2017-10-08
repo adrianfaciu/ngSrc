f0cd5db3eacb0086d014a225ed843b5ad63e9c70
Adrian Fâciu <adrian.faciu@gmail.com>
2017-10-08 12:22:35 +0300

    Working on using xmlHttpRequest to load the image

--- src/retriable-src/retriable-src.directive.ts
+++ src/retriable-src/retriable-src.directive.ts
@@ -17,9 +17,41 @@ export class RetriableSrcDirective implements OnChanges {
   @HostBinding('src') imgSrc;
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log(changes);
+    if (this.validateUrl(this.sourceUrl)) {
+      this.fetchImage(this.sourceUrl);
+    }
+  }
 
-    this.imgSrc = this.sourceUrl;
+  validateUrl(url: string) {
+    // todo validate the url
+    return true;
+  }
+
+  fetchImage(url: string) {
+    console.log(url);
+
+    const httpRequest = new XMLHttpRequest();
+
+    httpRequest.open('GET', url, true);
+    httpRequest.responseType = 'arraybuffer';
+    httpRequest.onload = this.onRequestLoaded;
+
+    httpRequest.send();
+  }
+
+  onRequestLoaded(ev: ProgressEvent): any {
+    console.log('Response: ', ev);
+    const request = ev.target as XMLHttpRequest;
+
+    const arrayBufferView = new Uint8Array(request.response);
+    const blob = new Blob([ arrayBufferView ], { type: 'image/jpeg' });
+    const imageUrl = URL.createObjectURL(blob);
+
+    this.imgSrc = imageUrl;
+
+    // Get the description from S3 metadata
+    // var desc = this.getResponseHeader('x-amz-meta-description');
+    // img.setAttribute('data-description', desc);
   }
 }
 
@@ -27,4 +59,6 @@ export class RetriableSrcDirective implements OnChanges {
 // Handle case when sourceUrl is not an URl
 
 // Should we load with XMLHttpRequest in order not to depend on HttpClient
-// Might make this optional and inject HttpClient or other service
+// Might make this optional and inject HttpClient or other service ?
+
+// Four spaces instead of two
